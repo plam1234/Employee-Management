161fb6dfce0f7d6bc8aeb9996410f3d2f73319e9
Peter Lam <Piitaa1234@gmail.com>
2020-06-15 19:28:46 -0400

    fixed db

--- employeeTracker.js
+++ employeeTracker.js
@@ -2,27 +2,27 @@ var inquirer = require("inquirer");
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "q3vtafztappqbpzn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 
   // Your port; if not 3306
   port: 3306,
 
   // Your username
-  user: "root",
+  user: "yk3ojstgfkh3nkx5",
 
   // Your password
-  password: "jy2052927",
-  database: "employees_DB"
+  password: "mvf8482rsi21bv7",
+  database: "b1pvzn6vwvgbme1d",
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
   askUserChoice();
-  });
+});
 
 function askUserChoice() {
-    inquirer
+  inquirer
     .prompt({
       name: "action",
       type: "list",
@@ -35,45 +35,42 @@ function askUserChoice() {
         "View roles",
         "View employees",
         "Update employee role",
-        "Exit"
-      ]
+        "Exit",
+      ],
     })
-    .then(function(answer) {
+    .then(function (answer) {
       switch (answer.action) {
-      case "Add a department":
-        addDepartment();
-        break;
+        case "Add a department":
+          addDepartment();
+          break;
 
-      case "Add an employee role":
-        addRole();
-        break;
+        case "Add an employee role":
+          addRole();
+          break;
 
-      case "Add employee":
-        addEmployee();
-        break;
+        case "Add employee":
+          addEmployee();
+          break;
 
-      case "View departments":
-        viewDepartments();
-        break;
-      
-      case "View roles":
-        viewRoles();
-        break;
+        case "View departments":
+          viewDepartments();
+          break;
 
-      case "View employees":
-        viewEmployees();
-        break;
+        case "View roles":
+          viewRoles();
+          break;
 
-      case "Update employee role":
-        updateEmployee();
-        break;
+        case "View employees":
+          viewEmployees();
+          break;
 
-      case "exit":
-        connection.end();
-        break;
+        case "Update employee role":
+          updateEmployee();
+          break;
+
+        case "exit":
+          connection.end();
+          break;
       }
     });
 }
-
-
-
